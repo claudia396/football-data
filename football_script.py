60a2891f3fb863d46d98b9c67da635fd1b72724b
claudia396 <61069975+claudia396@users.noreply.github.com>
2026-04-20 13:14:43 -0500

    Update football_script.py

--- football_script.py
+++ football_script.py
@@ -4,6 +4,7 @@ from extract_matches import get_matches
 from extract_stats import get_stats
 from load_sheets import connect_sheets
 from utils import setup_logging
+import pandas as pd
 
 def main():
 
@@ -15,9 +16,19 @@ def main():
     df_matches = df_matches.fillna("")
     df_matches["date"] = df_matches["date"].astype(str)
 
-    match_ids = df_matches["match_id"].tolist()
+    # 2. sheets
+    ws_matches, ws_stats = connect_sheets(
+        "creds.json",
+        SPREADSHEET_ID
+    )
 
-    # 2. stats
+    existing_matches = ws_matches.get_all_records()
+    existing_matches = pd.DataFrame(existing_matches)
+    new_matches = df_matches[~df_matches["match_id"].isin(existing_matches.get("match_id", []))]
+
+    match_ids = new_matches["match_id"].tolist()
+	
+    # 3. stats
     df_stats = get_stats(HEADERS, match_ids)
 
     df_stats_filtered = df_stats[df_stats["period"] == "ALL"].copy()
@@ -32,22 +43,10 @@ def main():
 
     df_stats_filtered = df_stats_filtered.fillna("")
 
-    # 3. sheets
-    ws_matches, ws_stats = connect_sheets(
-        "creds.json",
-        SPREADSHEET_ID
-    )
-
     # 4. incremental update
-    existing_matches = ws_matches.get_all_records()
+
     existing_stats = ws_stats.get_all_records()
-
-    import pandas as pd
-
-    existing_matches = pd.DataFrame(existing_matches)
     existing_stats = pd.DataFrame(existing_stats)
-
-    new_matches = df_matches[~df_matches["match_id"].isin(existing_matches.get("match_id", []))]
     new_stats = df_stats_filtered[~df_stats_filtered["match_id"].isin(existing_stats.get("match_id", []))]
 
     if not new_matches.empty:
